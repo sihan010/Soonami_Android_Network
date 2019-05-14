4607eed55b26afbf7a1f7f19ed775d4466cf13ff
Manal IT <manal@m.insta.kim>
2019-05-14 12:46:23 +0600

    initial commit

---
+++ app/src/main/java/com/sihan/android/soonami/Event.java
@@ -0,0 +1,14 @@
+package com.sihan.android.soonami;
+
+public class Event {
+    public final String title;
+    public final long time;
+    public final int tsunamiAlert;
+
+    public Event(String eventTitle, long eventTime, int eventTsunamiAlert) {
+        title = eventTitle;
+        time = eventTime;
+        tsunamiAlert = eventTsunamiAlert;
+    }
+}
+
